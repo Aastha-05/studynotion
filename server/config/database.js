186c8a1f2065cbfab1dee3f54de10e7fe6a1bf36
Aastha-05 <aasthasaxena05a@gmail.com>
2024-08-23 14:22:27 +0530

    first commit

---
+++ server/config/database.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology:true,
+    })
+    .then(() => console.log("DB Connected Successfully"))
+    .catch( (error) => {
+        console.log("DB Connection Failed");
+        console.error(error);
+        process.exit(1);
+    } )
+};
+
+
+// require("dotenv").config();
+
+// exports.connect = () => {
+//     mongoose.connect(process.env.MONGODB_URL, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology:true,
+//     })
+//     .then(() => console.log("DB Connected Successfully"))
+//     .catch( (error) => {
+//         console.log("DB Connection Failed");
+//         console.error(error);
+//         process.exit(1);
+//     } )
+// };
