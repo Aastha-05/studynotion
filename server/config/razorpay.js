a470584757167086dae5801d0ee8d6ade95b7b7b
Aastha Saxena <145255708+Aastha-05@users.noreply.github.com>
2024-08-23 20:11:31 +0530

    Update razorpay.js

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,19 +1,5 @@
 const Razorpay = require("razorpay");
 
-// exports.instance = new Razorpay({
-// 	key_id: process.env.RAZORPAY_KEY,
-// 	key_secret: process.env.RAZORPAY_SECRET,
-// });
-
-// const RAZORPAY_KEY = "rzp_test_fUIZAI25WMgGwi"
-
-// const RAZORPAY_SECRET = "o7TBIxOogcPsNQa9pTzsAoYf"
-
-// exports.instance = new Razorpay({
-// 	key_id: RAZORPAY_KEY,
-// 	key_secret: RAZORPAY_SECRET,
-// });
-
 exports.instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY,
     key_secret: process.env.RAZORPAY_SECRET,
