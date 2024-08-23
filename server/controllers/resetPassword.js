2228b3fc423f8a9c902b3a474cdbb78ff08e4331
Aastha-05 <aasthasaxena05a@gmail.com>
2024-08-23 15:35:49 +0530

    URL update

--- server/controllers/resetPassword.js
+++ server/controllers/resetPassword.js
@@ -24,7 +24,7 @@ exports.resetPasswordToken = async (req, res) => {
     )
     console.log("DETAILS", updatedDetails)
 
-    const url = `http://localhost:3000/update-password/${token}`
+    const url = `https://studynotion-by-aastha.vercel.app/update-password/${token}`
     // const url = `https://studynotion-edtech-project.vercel.app/update-password/${token}`
 
     await mailSender(
--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: "https://studynotion-by-aastha.vercel.app",
 		credentials: true,
 	})
 );
